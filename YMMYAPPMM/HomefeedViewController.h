c060064a4b463716f32fea80dec5421604faae8b
Kevin <Kevin@Kevins-MacBook-Pro.local>
2015-02-11 11:11:55 -0800

    initial commit and starting point

---
+++ YMMYAPPMM/HomefeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomefeedViewController.h
+//  YMMYAPPMM
+//
+//  Created by Kevin on 2/11/15.
+//  Copyright (c) 2015 Kevin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomefeedViewController : UIViewController
+
+@end
---
+++ YMMYAPPMM/HomefeedViewController.m
@@ -0,0 +1,13 @@
+//
+//  HomefeedViewController.m
+//  YMMYAPPMM
+//
+//  Created by Kevin on 2/11/15.
+//  Copyright (c) 2015 Kevin Lee. All rights reserved.
+//
+
+#import "HomefeedViewController.h"
+
+@implementation HomefeedViewController
+
+@end
